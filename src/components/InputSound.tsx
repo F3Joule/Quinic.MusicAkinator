77760621aa80f4f3a5781c559225b43dfab9162f
Vlad Proshchavaiev <f3joule@gmail.com>
2020-01-25 16:55:02 +0200

    Fix search input

--- src/components/InputSound.tsx
+++ src/components/InputSound.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { ReactMediaRecorder } from 'react-media-recorder';
+import { TextField, IconButton } from '@material-ui/core';
 import MicIcon from '@material-ui/icons/Mic';
 
 export const InputSound = () => {
+  const [lyrics, setLyrics] = useState<string | null>(null);
+  var isRecording = false;
+
   return (
     <div className='Input-music'>
-      <TextField id='standard-basic' label='Hint Text' />
-      <div className='icon'>
-        <MicIcon />
-      </div>
+      <TextField
+        id='lyricsInput'
+        label='Input lyrics part'
+        required
+        onChange={event => {
+          setLyrics(event.target.value);
+        }}
+      />
+      <ReactMediaRecorder
+        audio
+        render={({ status, startRecording, stopRecording }) => (
+          <div className='icon'>
+            <IconButton
+              onClick={() => {
+                isRecording = !isRecording;
+              }}>
+              <MicIcon style={{ color: 'white' }} />
+            </IconButton>
+          </div>
+        )}
     </div>
   );
 };
